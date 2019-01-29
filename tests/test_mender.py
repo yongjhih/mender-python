073d9db110011a315b2d1aac7b54c7434d5f487d
Andrew Chen <yongjhih@gmail.com>
2019-01-29 16:04:20 +0800

    Remove unused code

--- tests/test_mender.py
+++ tests/test_mender.py
@@ -10,19 +10,15 @@ import uuid
 import os
 import json
 import pytest
-from flo.echo_web_socket_server import EchoWebSocketServer
 from unittest.mock import MagicMock
 
 import ssl
 
-import threading
 from typing import Any, Callable, Dict, List, Optional, Union, NewType, Iterable, TypeVar
 
 from _pytest.fixtures import SubRequest
 
 @pytest.mark.asyncio
-async def test_SimpleFloDevice_async_login(server):
-    server.echo_data = '''{}'''
-
+async def test_login():
     res = '''{}''' # Mender.device_get(id = "")
     assert '''{}''' == res
