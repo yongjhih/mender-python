425e72422a6c364a4f7fc65fa70a9013be782335
Andrew Chen <yongjhih@gmail.com>
2019-01-30 15:19:38 +0800

    Attribute: Support List within value

--- mender/attribute.py
+++ mender/attribute.py
@@ -1,6 +1,6 @@
 # -*- coding: future_fstrings -*-
 
-from typing import Optional
+from typing import Optional, Union, List
 
 from dataclasses import dataclass
 
@@ -11,7 +11,7 @@ class Attribute:
     """
     A human readable, unique attribute ID, e.g. &#39;device_type&#39;, &#39;ip_addr&#39;, &#39;cpu_load&#39;, etc. 
     """
-    value: str
+    value: Union[str, List]
     """
     The current value of the attribute.  Attribute type is implicit, inferred from the JSON type.  Supported types: number, string, array of numbers, array of strings. Mixed arrays are not allowed. 
     """
