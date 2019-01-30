834d9aaf56c77f50d31a7b4201f5a3916a2ed4de
Andrew Chen <yongjhih@gmail.com>
2019-01-30 15:24:19 +0800

    Update tests

--- tests/test_mender.py
+++ tests/test_mender.py
@@ -9,7 +9,7 @@ from mender import Mender
 
 @pytest.mark.asyncio
 async def test_login():
-    mender = Mender(base_url=os.environ["MENDER_URL"], session=aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)))
+    mender = Mender(base_url=os.environ["MENDER_URL"], session=aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)))
     token = await mender.login(os.environ["MENDER_USERNAME"], os.environ["MENDER_PASSWORD"])
     assert None != token
     devices = await mender.get_devices_paged()
